9104d91914ce2562ea7da68c0c07f71318db7274
Mateusz Lewczak <leftarcode@gmail.com>
2023-06-18 23:38:52 +0200

    Add default Spring Web application

---
+++ spring-support/src/main/java/pl/securitum/vulnerable/spring/DamnVulnerableSpring/DamnVulnerableSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.securitum.vulnerable.spring.DamnVulnerableSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamnVulnerableSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamnVulnerableSpringApplication.class, args);
+	}
+
+}

a6deacd755d40e0873a30b7a58e2d68bdf06da2a
Mateusz Lewczak <leftarcode@gmail.com>
2023-06-18 23:51:22 +0200

    Create vulnerable API methods

--- spring-support/src/main/java/pl/securitum/vulnerable/spring/DamnVulnerableSpring/DamnVulnerableSpringApplication.java
+++ spring-support/src/main/java/pl/securitum/vulnerable/spring/DamnVulnerableSpring/DamnVulnerableSpringApplication.java
@@ -2,12 +2,41 @@ package pl.securitum.vulnerable.spring.DamnVulnerableSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.util.Base64;
 
 @SpringBootApplication
+@RestController
 public class DamnVulnerableSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DamnVulnerableSpringApplication.class, args);
 	}
 
+	@PostMapping("/support")
+	public String createSupportTicket(@RequestParam String problem, @RequestParam String description) throws IOException {
+		TicketModel ticket = new TicketModel(problem, description);
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream( baos );
+		oos.writeObject( ticket );
+		oos.close();
+
+		return Base64.getEncoder().encodeToString(baos.toByteArray());
+	}
+	@PostMapping("/support/confirm")
+	public String confirmSupportTicket(@RequestParam String confirmation) throws IOException, ClassNotFoundException {
+		byte [] data = Base64.getDecoder().decode(confirmation);
+		ObjectInputStream ois = new ObjectInputStream(
+				new ByteArrayInputStream(  data ) );
+		TicketModel ticket  = (TicketModel) ois.readObject();
+		ois.close();
+
+		return "confirmed";
+	}
 }
--- spring-support/src/main/java/pl/securitum/vulnerable/spring/DamnVulnerableSpring/TicketModel.java
+++ spring-support/src/main/java/pl/securitum/vulnerable/spring/DamnVulnerableSpring/TicketModel.java
@@ -1,2 +1,13 @@
-package pl.securitum.vulnerable.spring.DamnVulnerableSpring;public class TicketModel {
+package pl.securitum.vulnerable.spring.DamnVulnerableSpring;
+
+import java.io.Serializable;
+
+public class TicketModel implements Serializable {
+    private String problem;
+    private String description;
+
+    public TicketModel(String problem, String description) {
+        this.problem = problem;
+        this.description = description;
+    }
 }
